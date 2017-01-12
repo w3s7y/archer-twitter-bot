baf9b884667279538f13b9c603a0fa9a2e9727de
Scott Spence <scottspence10@gmail.com>
2017-01-12 22:06:58 +0000

    add post media and phrasing.js :question:

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,10 @@
 var
   twit = require('twit'),
-  config = require('./config'),
   ura = require('unique-random-array'),
-  strings = require('./helpers/strings');
+  fs  = require('fs'),
+  config = require('./config'),
+  strings = require('./helpers/strings'),
+  responses = require('./helpers/responses');
 
 
 var T = new twit(config);
@@ -44,7 +46,7 @@ var retweet = function() {
       });
     }
     else {
-      return console.log('CANNOT RETWEET! Derp!' + err);
+      return console.log('CANNOT RETWEET! Derp!', err);
     }
     var randomUser = randIdx(data.statuses).user.screen_name;
     console.log(randomUser + ' ' + paramQs + ' ' + paramRt);
@@ -107,9 +109,70 @@ favorite();
 setInterval(favorite, 60000 * favoriteFrequency);
 
 
-// Random index
+// ====================================
+//    STREAM API
+// ====================================
+var userStream = T.stream('user');
+
+userStream.on('follow', followed);
+
+function followed(event) {
+  console.log('FOLLOW EVENT RUNNING');
+  
+  // get USER's twitter handler (screen name)
+  var screenName = event.source.screen_name;
+  
+  // function that replies back to every USER who followed for the first time
+  tweetNow('Thanks @' + screenName + ' you rock!');
+  
+}
+
+
+function tweetNow(tweetTxt) {
+  var tweet = {
+    status: tweetTxt
+  };
+  T.post('statuses/update', tweet, function(err, data, response) {
+    if (err) {
+      console.log('REPLY DERP! ERROR!', err);
+    }
+    else {
+      console.log('REPLY SUCCESS!');
+    }
+  });
+}
+
+
+// Random index === possibly move this to helper folder
 
 function randIdx(arr) {
   var index = Math.floor(Math.random() * arr.length);
   return arr[index];
 }
+
+
+// ====================================
+//    POST TWEET WITH MEDIA 
+// ====================================
+var b64content = fs.readFileSync('./src/archer.png', { encoding: 'base64' });
+ 
+// first we must post the media to Twitter 
+T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+  // now we can assign alt text to the media, for use by screen readers and 
+  // other text-based presentations and interpreters 
+  var mediaIdStr = data.media_id_string;
+  var altText = "Small flowers in a planter on a sunny balcony, blossoming.";
+  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
+ 
+  T.post('media/metadata/create', meta_params, function (err, data, response) {
+    if (!err) {
+      // now we can reference the media and post a tweet (media will attach to the tweet) 
+      var params = { status: 'loving life #nofilter', media_ids: [mediaIdStr] };
+ 
+      T.post('statuses/update', params, function (err, data, response) {
+        console.log(data, 'YAY!');
+      });
+    }
+  });
+});
+
---
+++ src/helpers/phrasing.js
@@ -0,0 +1,27 @@
+// ## Phrasing?
+
+// - Are you coming?
+
+// - Something, something, danger zone, I know I'm not even trying any more
+
+// - I like your spunk
+
+// - I'm coming for you
+
+// ## That's how you get ants
+
+// - Is that what you want? Because that's how you get ants
+
+// ## Danger zone
+
+// - Lana?, Lana?, Lana?, LANAAAAAAAAAAAAA!
+
+// - It will be dangerous. Would you say that we would be entering into a 'zone' of danger?
+
+// ## Rampage
+
+// - RAMPAGE!
+
+// ## Just the tip
+
+// When someone really needs to try something.
