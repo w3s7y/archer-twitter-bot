5692db07d3008cf4260e1d6ad03290b679a677cf
spences10 <spences10apps@gmail.com>
2017-05-10 09:28:05 +0100

    reduce responses

--- src/helpers/responses.js
+++ src/helpers/responses.js
@@ -1,9 +1,3 @@
 module.exports = function(screenName) {
-    `Hi @${screenName} thanks for the follow! What are you working on today? .CR`,
-    `@${screenName} thanks for following! What are you working on today? .CR`,
-    `Hey @${screenName} thanks for the follow! What are you working on today? .CR`,
-    `Thanks for following @${screenName}! What are you working on today? .CR`,
-    `Thanks for following @${screenName}! I look forward to tweeting with you. .CR`,
-    `Hey @${screenName}, working on anything code related today? Thanks for following! .CR`,
     `Awesome @${screenName}, thanks for following!`;
 };
