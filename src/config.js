263740397d70c218fd07ea9f39fc8971e46a041e
spences10 <spences10apps@gmail.com>
2017-05-10 08:38:25 +0100

    use dotenv

---
+++ src/config.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+module.exports = {
+  twitter: {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    username: process.env.TWITTER_USERNAME,
+    retweet: process.env.TWITTER_RETWEET_RATE,
+    favorite: process.env.TWITTER_FAVORITE_RATE
+  },
+  sentiment_api_key: process.env.SENTIMENT_KEY,
+  queryString: process.env.QUERY_STRING
+}
